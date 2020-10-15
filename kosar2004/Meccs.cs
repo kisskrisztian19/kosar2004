02ed8aefa7111748d2c103544b1312ae00245dc1
krisztián kiss <kisskrisztiannnnn@hotmail.com>
2020-10-15 09:20:07 +0200

    Meccs osztály elkészítése

---
+++ kosar2004/Meccs.cs
@@ -0,0 +1,23 @@
+﻿namespace kosar2004
+{
+    internal class Meccs
+    {
+        public string Hazai { get; private set; }
+        public string Idegen { get; private set; }
+        public int HPont { get; private set; }
+        public int IPont { get; private set; }
+        public string Hely { get; private set; }
+        public string Ido { get; private set; }
+        
+
+        public Meccs(string Hazai, string Idegen, int HPont, int IPont, string Hely, string Ido)
+        {
+            this.Hazai = Hazai;
+            this.Idegen = Idegen;
+            this.HPont = HPont;
+            this.IPont = IPont;
+            this.Hely = Hely;
+            this.Ido = Ido;
+        }
+    }
+}
--- kosar2004/Program.cs
+++ kosar2004/Program.cs
@@ -10,7 +10,15 @@ namespace kosar2004
     {
         static void Main(string[] args)
         {
+            // 7up Joventut;
+            // Adecco Estudiantes;81;73;
+            // Palacio Mun. De Deportes De Badalona;2005-04-03
 
+            var m = new Meccs("7up Joventut", "Adecco Estudiantes", 81, 73, "Palacio Mun. De Deportes De Badalona", "2005 - 04 - 03");
+
+            Console.WriteLine($"{m.Hazai} - {m.Idegen} ({m.HPont}:{m.IPont})");
+
+            Console.ReadLine();
         }
     }
 }
